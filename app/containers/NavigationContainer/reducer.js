e35ec01b5bee4f634866e5c0c68e65208e9ffcc1
luanatsainsburys <luan.au@sainsburys.co.uk>
2017-06-14 11:47:56 +0100

    Add 'requestTopics' saga

--- app/containers/NavigationContainer/reducer.js
+++ app/containers/NavigationContainer/reducer.js
@@ -6,15 +6,27 @@
 
 import { fromJS } from 'immutable';
 import {
-  DEFAULT_ACTION,
 } from './constants';
 
-const initialState = fromJS({});
+const initialState = fromJS({
+  topics: [
+    {
+      name: 'libraries',
+      description: 'Links to useful open source libraries',
+    },
+    {
+      name: 'apps',
+      description: 'Links to new and exciting apps',
+    },
+    {
+      name: 'news',
+      description: 'Links to programming related news articles',
+    }
+  ],
+});
 
 function navigationContainerReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
     default:
       return state;
   }
--- app/containers/NavigationContainer/sagas.js
+++ app/containers/NavigationContainer/sagas.js
@@ -1,11 +1,24 @@
+import { REQUEST_TOPICS } from './constants';
+import { takeLatest } from 'redux-saga';
+import { call } from 'redux-saga/effects';
+
 // import { take, call, put, select } from 'redux-saga/effects';
 
+export function fetchTopicsFromServer() {
+  return fetch('http://localhost:3000/api/topics')
+    .then(response => response.json());
+}
+
+function* fetchTopics() {
+  const topics = yield call(fetchTopicsFromServer);
+  console.log('TOPICS FROM THE SERVER!!!', topics);
+}
 // Individual exports for testing
-export function* defaultSaga() {
-  return;
+export function* fecthTopicsSaga() {
+  yield* takeLatest(REQUEST_TOPICS, fetchTopics);
 }
 
 // All sagas to be loaded
 export default [
-  defaultSaga,
+  fecthTopicsSaga,
 ];
